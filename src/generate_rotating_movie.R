1d3306f68e3c5339a19cd9b180f9f82573bdb71a
Benjamin Cole <35984229+b-coli@users.noreply.github.com>
2022-07-25 11:24:10 -0700

    Update generate_rotating_movie.R

--- src/generate_rotating_movie.R
+++ src/generate_rotating_movie.R
@@ -10,19 +10,17 @@ generate_rotating_movie <- function(sobj, color_scheme, filename = "movie.gif") 
     dplyr::mutate(UMAP3D_1 = UMAP3D_1 - com[1],
                   UMAP3D_2 = UMAP3D_2 - com[2],
                   UMAP3D_3 = UMAP3D_3 - com[3])
+    
+  message("Generating frame data...")
 
   cell_mat <- cell_data %>% 
     dplyr::select(Cell, UMAP3D_1, UMAP3D_2, UMAP3D_3) %>% 
     tibble::column_to_rownames("Cell") %>% 
     as.matrix()
-  movie_data <- purrr::map_dfr(1:360, .f = function(angle) {
-    print(angle)
-    frame <- to_grid(cell_mat, angle)
-    return(frame)
-  })
-
-  movie_data <- dplyr::left_join(movie_data, cell_data)
-
+    
+  movie_data <- purrr::map_dfr(1:360, to_grid, mat = cell_mat)
+  movie_data <- left_join(movie_data, cell_data, by = "Cell")
+    
   movie <- animate_movie(movie_data, color_scheme, filename = filename)
   
   return(movie)
