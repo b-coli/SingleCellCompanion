914a46a54a6d651127cee3a5a31e6b50492cf3ef
Benjamin Cole <35984229+b-coli@users.noreply.github.com>
2022-07-25 11:24:37 -0700

    Update animate_movie.R

--- src/animate_movie.R
+++ src/animate_movie.R
@@ -1,14 +1,11 @@
-
 animate_movie <- function(movie_data, color_scheme, filename = "movie.gif") {
-  anim <- ggplot2::ggplot(movie_data %>% 
-			  dplyr::mutate(Cell_Type = if_else(Cell_Type == "Putative Quiescent Center", 
-							    "Quiescent Center", 
-							    as.character(Cell_Type))) %>% 
-			  dplyr::arrange(depth), ggplot2::aes(x = x, y = y)) +
+  message("Generating anim object...")
+
+  anim <- ggplot2::ggplot(dplyr::arrange(movie_data, depth), ggplot2::aes(x = x, y = y)) +
     ggplot2::geom_point(ggplot2::aes(size = depth, color = Cell_Type)) +
     ggplot2::coord_fixed() + 
     ggplot2::scale_color_manual(values = color_scheme) +
-    ggplot2::scale_size_continuous(range = c(0.02, 0.1), guide = FALSE) +
+    ggplot2::scale_size_continuous(range = c(0.02, 0.1), guide = "none") +
     ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 2))) +
     gganimate::transition_states(angle, transition_length = 1, state_length = 0) +
     ggplot2::theme(
@@ -22,8 +19,14 @@ animate_movie <- function(movie_data, color_scheme, filename = "movie.gif") {
       legend.text = ggplot2::element_text(color = "white", size = 16)
     )
   nframes <- movie_data %>% pull(angle) %>% unique() %>% length()
+	
+  message("Rendering...")
+	
   movie = gganimate::animate(anim, nframes = nframes, fps = 25, width = 1200, height = 800, 
                              renderer = gganimate::magick_renderer())
+	
+  message("Saving...")
+	
   movie <- gganimate::anim_save(filename = filename, animation = movie, path = ".")
   return(movie)
 }
